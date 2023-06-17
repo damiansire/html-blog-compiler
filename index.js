d4c0b7120723f7dac9434c068c6da7d2c98c6c32
Damian Sire <damiansire@hotmail.com>
2023-06-17 16:47:23 -0300

    feat; agrego los modos de ejecucion default y watch

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 //Run file compiler
 const { runCompiler } = require("./src/libs/compiler");
-runCompiler();
+
+const modo = process.argv[2]; // Obtener el tercer argumento de la línea de comandos
+
+if (modo === "watch") {
+  // Lógica para el modo 1
+  console.log("Ejecutando en modo watch");
+  require("./src/libs/observer");
+} else {
+  console.log("Ejecutando en modo default");
+  runCompiler();
+}
