f0eac238fd75974d7c345a3d44d46c62ad61d5d2
Damian Sire <damiansire@hotmail.com>
2023-06-17 14:23:23 -0300

    feat: se obtiene la informacion de archivos y subarchivos en un directorio

--- compiler/dir-parser.js
+++ compiler/dir-parser.js
@@ -3,23 +3,32 @@ const path = require("path");
 
 function obtenerNombresArchivosYCarpetas(rutaProyecto, rutaRelativa = "") {
   const archivos = fs.readdirSync(rutaProyecto);
+  const elementos = [];
 
   archivos.forEach((archivo) => {
     const rutaArchivo = path.join(rutaProyecto, archivo);
     const stats = fs.statSync(rutaArchivo);
+    const elemento = {
+      nombre: archivo,
+      ruta: `${rutaRelativa}/${archivo}`,
+      esCarpeta: stats.isDirectory(),
+    };
+
+    elementos.push(elemento);
 
     if (stats.isDirectory()) {
-      console.log(`Carpeta: ${rutaRelativa}/${archivo}`);
-      obtenerNombresArchivosYCarpetas(
+      const subElementos = obtenerNombresArchivosYCarpetas(
         rutaArchivo,
         `${rutaRelativa}/${archivo}`
       ); // Llamada recursiva para explorar subcarpetas
-    } else if (stats.isFile()) {
-      console.log(`Archivo: ${rutaRelativa}/${archivo}`);
+      elementos.push(...subElementos);
     }
   });
+
+  return elementos;
 }
 
 // Ejemplo de uso
 const rutaProyecto = "../blog";
-obtenerNombresArchivosYCarpetas(rutaProyecto);
+const data = obtenerNombresArchivosYCarpetas(rutaProyecto);
+console.log(data);
